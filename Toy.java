2fe98eee6e955e04c79542b428060a940d6d5e6b
Your Name <you@example.com>
2024-06-25 17:39:34 +0400

    добавили код

---
+++ Toy.java
@@ -0,0 +1,37 @@
+public class Toy {
+    private String id;
+    private String name;
+    private int weight;
+
+    public Toy(String id, String name, int weight) {
+        this.id = id;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Toy{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+}
+
